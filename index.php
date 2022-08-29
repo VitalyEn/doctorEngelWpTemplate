535263ae7ec164c9c38f229107c36e61c5a43bb1
Vitaly Engel <vitaliy.engel@gmail.com>
2022-08-29 18:02:28 +0300

    drEngel theme restart

--- index.php
+++ index.php
@@ -1,57 +1,192 @@
-<?php
-/**
- * The main template file
- *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
- *
- * @package doctorengel
- */
+<!DOCTYPE html>
+<html lang="en">
 
-get_header();
-?>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Doctor Engel</title>
 
-	<main id="primary" class="site-main">
+    <!-- Подключаем стили проекта -->
+    <link rel="stylesheet" href="css/app.min.css">
+    <link href="https://allfont.ru/allfont.css?fonts=lora" rel="stylesheet" type="text/css" />
+    <?php
+        wp_head();
+    ?>
+</head>
 
-		<?php
-		if ( have_posts() ) :
+<body class="page-body wrapper">
+    <header class="page-header  ">
+        <div class="page-header__wrapper wrapper-in ">
+            <!-- main-nav--closed main-nav--nojs -->
+            <nav class="main-nav ">
+                <button class="main-nav__toggle " type="button " aria-label="Открыть меню ">
+                </button>
+                <a class="page-header__logo ">Доктор Энгель</a>
+                <ul class="main-nav__list list-style ">
+                    <li class="main-nav__item main-nav__item--active ">
+                        <a>Главная</a>
+                    </li>
+                    <li class="main-nav__item ">
+                        <a href="about.html ">Обо мне</a>
+                    </li>
+                    <li class="main-nav__item ">
+                        <a href="posts.html ">
+                        Статьи
+                        </a>
+                    </li>
+                    <li class="main-nav__item ">
+                        <a href="form.html ">
+                        Записаться на консультацию
+                        </a>
+                    </li>
+                    <li class="main-nav__item ">
+                        <a href="contacts.html ">
+                        Контакты
+                        </a>
+                    </li>
+                </ul>
+            </nav>
 
-			if ( is_home() && ! is_front_page() ) :
-				?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
-				<?php
-			endif;
+            <p class="page-header__title">Доктор Энгель</p>
 
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
+            <p class="page-header__slogan">Ваш путь к здоровой жизни!</p>
+        </div>
+    </header>
 
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_type() );
+    <main class="page-main wrapper-in">
+        <div>
+            <h2 class="featured-posts__header">Featured posts</h2>
+            <div class="featured-posts__line"></div>
+            <ul class="featured-post__list">
+                <li class="featured-post__container">
+                    <img class="featured-post__image" src="images/dest/fposts 1.png" alt="post 1">
+                    <h3 class="featured-post__header">The Road Ahead</h3>
+                    <p class="featured-post__slogan">The road ahead might be paved - it might not be.</p>
+                    <p class="featured-post__author">Mat Vogels</p>
+                    <p class="featured-post__date">September 25, 2015</p>
+                </li>
+                <li class="featured-post__container">
+                    <img class="featured-post__image" src="images/dest/fposts 2.png" alt="post 2">
+                    <h3 class="featured-post__header">From Top Down</h3>
+                    <p class="featured-post__slogan">Once a year, go someplace you’ve never been before.</p>
+                    <p class="featured-post__author">William Wong</p>
+                    <p class="featured-post__date">September 25, 2015</p>
+                </li>
+            </ul>
+        </div>
+        <div>
+            <h2 class="most-recent__header">Most Recent</h2>
+            <div class="featured-posts__line"></div>
+            <ul class="most-recent__list">
+                <li class="most-recent__container">
+                    <img class="most-recent__image" src="images/dest/mr 1.png" alt="post 1">
+                    <div class="most-recent__slogancontainer">
+                        <h3 class="most-recent__post-header">Still Standing Tall</h3>
+                        <p class="most-recent__slogan">Life begins at the end of your comfort zone.</p>
+                    </div>
+                    <div class="most-recent__datecontainer">
+                        <p class="most-recent__author">William Wong</p>
+                        <p class="most-recent__date">9/25/2015</p>
+                    </div>
+                </li>
+                <li class="most-recent__container">
+                    <img class="most-recent__image" src="images/dest/mr 2.png" alt="post 2">
+                    <div class="most-recent__slogancontainer">
+                        <h3 class="most-recent__post-header">Sunny Side Up</h3>
+                        <p class="most-recent__slogan">No place is ever as bad as they tell you it’s going to be.</p>
+                    </div>
+                    <div class="most-recent__datecontainer">
+                        <p class="most-recent__author ">William Wong</p>
+                        <p class="most-recent__date ">9/25/2015</p>
+                    </div>
 
-			endwhile;
+                </li>
+                <li class="most-recent__container ">
+                    <img class="most-recent__image " src="images/dest/mr 3.png " alt="post 3">
+                    <div class="most-recent__slogancontainer ">
+                        <h3 class="most-recent__post-header ">Water Falls</h3>
+                        <p class="most-recent__slogan ">We travel not to escape life, but for life not to escape us.</p>
+                    </div>
+                    <div class="most-recent__datecontainer">
+                        <p class="most-recent__author ">William Wong</p>
+                        <p class="most-recent__date ">9/25/2015</p>
+                    </div>
 
-			the_posts_navigation();
+                </li>
+                <li class="most-recent__container ">
+                    <img class="most-recent__image " src="images/dest/mr 4.png " alt="post 4">
+                    <div class="most-recent__slogancontainer ">
+                        <h3 class="most-recent__post-header ">Through the Mist</h3>
+                        <p class="most-recent__slogan ">Travel makes you see what a tiny place you occupy in the world.</p>
+                    </div>
+                    <div class="most-recent__datecontainer">
+                        <p class="most-recent__author ">William Wong</p>
+                        <p class="most-recent__date ">9/25/2015</p>
+                    </div>
 
-		else :
+                </li>
+                <li class="most-recent__container ">
+                    <img class="most-recent__image " src="images/dest/mr 5.png " alt="post 5">
+                    <div class="most-recent__slogancontainer ">
+                        <h3 class="most-recent__post-header ">Awaken Early</h3>
+                        <p class="most-recent__slogan ">Not all those who wander are lost.</p>
+                    </div>
+                    <div class="most-recent__datecontainer">
+                        <p class="most-recent__author ">William Wong</p>
+                        <p class="most-recent__date ">9/25/2015</p>
+                    </div>
 
-			get_template_part( 'template-parts/content', 'none' );
+                </li>
+                <li class="most-recent__container ">
+                    <img class="most-recent__image " src="images/dest/mr 6.png " alt="post 6 ">
+                    <div class="most-recent__slogancontainer ">
+                        <h3 class="most-recent__post-header ">Try it Always</h3>
+                        <p class="most-recent__slogan ">The world is a book, and those who do not travel read only one page.</p>
+                    </div>
+                    <div class="most-recent__datecontainer ">
+                        <p class="most-recent__author ">William Wong</p>
+                        <p class="most-recent__date ">9/25/2015</p>
+                    </div>
 
-		endif;
-		?>
+                </li>
+            </ul>
+        </div>
+    </main>
+    <footer class="page-footer wrapper-in ">
+        <p class="page-footer__slogan ">Stay in Touch</p>
+        <div class="featured-posts__line "></div>
+        <form class="page-footer__form ">
+            <input class="form__input " placeholder="Enter your email address ">
+            <button class="button form__button ">Submit</button>
+        </form>
+        <nav class="footer-nav ">
+            <a class="footer__logo ">Доктор Энгель</a>
+            <ul class="footer-nav__list list-style ">
+                <li class="footer-nav__item footer-nav__item--active ">
+                    <a>Главная</a>
+                </li>
+                <li class="footer-nav__item ">
+                    <a href="about.html ">Обо мне</a>
+                </li>
+                <li class="footer-nav__item ">
+                    <a href="posts.html ">
+                    Статьи
+                    </a>
+                </li>
+                <li class="footer-nav__item ">
+                    <a href="form.html ">
+                    Записаться на консультацию
+                    </a>
+                </li>
+                <li class="footer-nav__item ">
+                    <a href="contacts.html ">
+                    Контакты
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    </footer>
+</body>
 
-	</main><!-- #main -->
-
-<?php
-get_sidebar();
-get_footer();
+</html>
