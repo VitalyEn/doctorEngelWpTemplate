99f2cbf76cb45102f506169f45e259b342e9fb33
Vitaly Engel <vitaliy.engel@gmail.com>
2022-08-31 18:38:15 +0300

    functions modified

--- functions.php
+++ functions.php
@@ -1,8 +1,12 @@
 <?php
+add_action('wp_enqueue_scripts', 'engel_styles');
 add_action('wp_enqueue_scripts', 'engel_scripts');
 
-function engel_scripts() {
+function engel_styles() {
     wp_enqueue_style('engel-style', get_stylesheet_uri());
+}
+
+function engel_scripts() {
     wp_enqueue_script('engel-scripts', get_template_directory_uri().'/assets/js/app.min.js', array(), null, true);
 }
 ?>
