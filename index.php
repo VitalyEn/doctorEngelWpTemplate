7d7c1c11447037076c0cf895d4e6d7d33f1284fe
Vitaly Engel <vitaliy.engel@gmail.com>
2022-08-31 18:51:58 +0300

    index paths fixed

--- index.php
+++ index.php
@@ -57,14 +57,14 @@
             <div class="featured-posts__line"></div>
             <ul class="featured-post__list">
                 <li class="featured-post__container">
-                    <img class="featured-post__image" src="images/dest/fposts 1.png" alt="post 1">
+                    <img class="featured-post__image" src="<?php echo bloginfo('template_url'); ?>/assets/images/dest/fposts 1.png" alt="post 1">
                     <h3 class="featured-post__header">The Road Ahead</h3>
                     <p class="featured-post__slogan">The road ahead might be paved - it might not be.</p>
                     <p class="featured-post__author">Mat Vogels</p>
                     <p class="featured-post__date">September 25, 2015</p>
                 </li>
                 <li class="featured-post__container">
-                    <img class="featured-post__image" src="images/dest/fposts 2.png" alt="post 2">
+                    <img class="featured-post__image" src="<?php echo bloginfo('template_url'); ?>/assets/images/dest/fposts 2.png" alt="post 2">
                     <h3 class="featured-post__header">From Top Down</h3>
                     <p class="featured-post__slogan">Once a year, go someplace you’ve never been before.</p>
                     <p class="featured-post__author">William Wong</p>
@@ -77,7 +77,7 @@
             <div class="featured-posts__line"></div>
             <ul class="most-recent__list">
                 <li class="most-recent__container">
-                    <img class="most-recent__image" src="images/dest/mr 1.png" alt="post 1">
+                    <img class="most-recent__image" src="<?php echo bloginfo('template_url'); ?>/assets/images/dest/mr 1.png" alt="post 1">
                     <div class="most-recent__slogancontainer">
                         <h3 class="most-recent__post-header">Still Standing Tall</h3>
                         <p class="most-recent__slogan">Life begins at the end of your comfort zone.</p>
@@ -88,7 +88,7 @@
                     </div>
                 </li>
                 <li class="most-recent__container">
-                    <img class="most-recent__image" src="images/dest/mr 2.png" alt="post 2">
+                    <img class="most-recent__image" src="<?php echo bloginfo('template_url'); ?>/assets/images/dest/mr 2.png" alt="post 2">
                     <div class="most-recent__slogancontainer">
                         <h3 class="most-recent__post-header">Sunny Side Up</h3>
                         <p class="most-recent__slogan">No place is ever as bad as they tell you it’s going to be.</p>
@@ -100,7 +100,7 @@
 
                 </li>
                 <li class="most-recent__container ">
-                    <img class="most-recent__image " src="images/dest/mr 3.png " alt="post 3">
+                    <img class="most-recent__image " src="<?php echo bloginfo('template_url'); ?>/assets/images/dest/mr 3.png " alt="post 3">
                     <div class="most-recent__slogancontainer ">
                         <h3 class="most-recent__post-header ">Water Falls</h3>
                         <p class="most-recent__slogan ">We travel not to escape life, but for life not to escape us.</p>
@@ -112,7 +112,7 @@
 
                 </li>
                 <li class="most-recent__container ">
-                    <img class="most-recent__image " src="images/dest/mr 4.png " alt="post 4">
+                    <img class="most-recent__image " src="<?php echo bloginfo('template_url'); ?>/assets/images/dest/mr 4.png " alt="post 4">
                     <div class="most-recent__slogancontainer ">
                         <h3 class="most-recent__post-header ">Through the Mist</h3>
                         <p class="most-recent__slogan ">Travel makes you see what a tiny place you occupy in the world.</p>
@@ -124,7 +124,7 @@
 
                 </li>
                 <li class="most-recent__container ">
-                    <img class="most-recent__image " src="images/dest/mr 5.png " alt="post 5">
+                    <img class="most-recent__image " src="<?php echo bloginfo('template_url'); ?>/assets/images/dest/mr 5.png " alt="post 5">
                     <div class="most-recent__slogancontainer ">
                         <h3 class="most-recent__post-header ">Awaken Early</h3>
                         <p class="most-recent__slogan ">Not all those who wander are lost.</p>
@@ -136,7 +136,7 @@
 
                 </li>
                 <li class="most-recent__container ">
-                    <img class="most-recent__image " src="images/dest/mr 6.png " alt="post 6 ">
+                    <img class="most-recent__image " src="<?php echo bloginfo('template_url'); ?>/assets/images/dest/mr 6.png " alt="post 6 ">
                     <div class="most-recent__slogancontainer ">
                         <h3 class="most-recent__post-header ">Try it Always</h3>
                         <p class="most-recent__slogan ">The world is a book, and those who do not travel read only one page.</p>
