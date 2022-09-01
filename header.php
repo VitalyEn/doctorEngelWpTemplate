6ed7b53207af150971afb50417b19ab64e715a5e
Vitaly Engel <vitaliy.engel@gmail.com>
2022-09-01 19:14:30 +0300

    title modified

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Doctor Engel</title>
+    <title><?php bloginfo('name'); echo" | "; bloginfo('description'); ?></title>
     <link href="https://allfont.ru/allfont.css?fonts=lora" rel="stylesheet" type="text/css" />
     <?php
         wp_head();
